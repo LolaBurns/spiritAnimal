edb39193cecfc9e80195aedda55394149c1dd2d9
Ashley Riehl and Lauren Burnett <ashley@>
2016-06-06 20:48:45 -0700

    format page, finish questions, everything else

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,24 @@ $(document).ready(function() {
 
     if (personality === "happy-go-lucky" && weather === "Hot") {
       $('#dog').show();
+      $('#cat').hide();
+      $('#polarbear').hide();
+      $('#dodo').hide();
     } else if (personality === "uptight" && activity === "Sleeping") {
         $('#cat').show();
+        $('#dog').hide();
+        $('#polarbear').hide();
+        $('#dodo').hide();
     } else if (weather === "Cold" && activity === "Swimming") {
         $('#polarbear').show();
+        $('#cat').hide();
+        $('#dog').hide();
+        $('#dodo').hide();
       } else {
         $('#dodo').show();
+        $('#cat').hide();
+        $('#polarbear').hide();
+        $('#dog').hide();
       }
 
     event.preventDefault();
